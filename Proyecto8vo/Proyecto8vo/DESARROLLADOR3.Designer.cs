0f5e539a6e16aa453be3cfeea48567f8b2a1d4bc
Desarrollador Angel <ingangel89_velazco@hotmail.com>
2021-01-27 12:50:01 -0600

    Union con el Desarrollador 3

---
+++ Proyecto8vo/Proyecto8vo/DESARROLLADOR3.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Proyecto8vo
+{
+    partial class DESARROLLADOR3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(101, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(402, 402);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos Generales";
+            // 
+            // DESARROLLADOR3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(633, 512);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "DESARROLLADOR3";
+            this.Text = "DESARROLLADOR3";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ Proyecto8vo/Proyecto8vo/DESARROLLADOR3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto8vo
+{
+    public partial class DESARROLLADOR3 : Form
+    {
+        public DESARROLLADOR3()
+        {
+            InitializeComponent();
+        }
+    }
+}
